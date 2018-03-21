118410d73b49c4aae2c90011b9ef3200069bdf92
AlexBHdez <abhbretones@gmail.com>
2018-03-21 17:04:20 +0100

    contact done

--- js/components/gmap.js
+++ js/components/gmap.js
@@ -22,7 +22,7 @@ window.initMap = function() {
   });
 
   var image = new google.maps.MarkerImage(
-  	'img/widgets/gmap-pin.png',
+  	'img/widgets/nopar-gmap-pin.png',
   	new google.maps.Size(48,54),
   	new google.maps.Point(0,0),
   	new google.maps.Point(24,54)
@@ -31,12 +31,13 @@ window.initMap = function() {
   var customMapTypeId = 'custom_style';
 
   var brooklyn = {lat: 41.850, lng: -73.961};
+  var noparSolutions = { lat: 41.403437114002074, lng: 2.137543800000003};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6,
+    zoom: 12,
     scrollwheel: false,
     streetViewControl: false,
     mapTypeControl: false,
-    center: brooklyn, // Brooklyn.
+    center: noparSolutions, // Muntaner, 494.
     mapTypeControlOptions: {
       mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
     }
@@ -45,23 +46,25 @@ window.initMap = function() {
   var contentString = '<div id="content">'+
     '<div id="siteNotice">'+
     '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">Brooklyn</h1>'+
+    '<h1 id="firstHeading" class="firstHeading">Nopar Solutions</h1>'+
     '<div id="bodyContent">'+
-    '<p>277 Bedford Avenue, <br> Brooklyn, NY 11211, <br> New York, USA</p>'+
+    '<p>Carrer Muntaner, 494 <br> Barcelona, 08022, España</p>'+
+    '<p><a href="tel:+34111222333"><i class="icon-screen-smartphone blue-color"></i> +34 111 222 333</a></p>'+
+    '<p><a href="mailto:hola@noparsolutions.com?subject=Contacto Web"><i class="icon-envelope blue-color"></i> hola@noparsolutions.com</a></p>'
     '</div>'+
     '</div>';
 
   var infowindow = new google.maps.InfoWindow({
     content: contentString,
-    maxWidth: 300
+    maxWidth: 350
   });
 
   var marker = new google.maps.Marker({
     map: map,
     clickable: true,
     icon: image,
-    title: 'Brooklyn',
-    position: brooklyn
+    title: 'NoparSolutions',
+    position: noparSolutions
   });
 
   marker.addListener('click', function() {
